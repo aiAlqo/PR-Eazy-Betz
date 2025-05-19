5270ad6ac19824091bf872d35f96e013eb4f96c2
Alois Seneka <56009073+aiAlqo@users.noreply.github.com>
2025-05-19 16:49:25 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,18 +19,3 @@ options_col9 = ['']
 # Table layout using columns
 with st.expander('NRL (National Rugby League)'):
     st.write("### Match No.1")
-
-# Loop over rows
-    for i in range(rows):
-        cols_ui = st.columns(cols)
-
-        for j in range(cols):
-            if j == 4:  # Column 5 (index 4)
-                with cols_ui[j]:
-                    st.radio(f"row_{i}_col5", options_col5, key=f"r{i}_c5", label_visibility="collapsed")
-            elif j == 8:  # Column 9 (index 8)
-                with cols_ui[j]:
-                    st.radio(f"row_{i}_col9", options_col9, key=f"r{i}_c9", label_visibility="collapsed")
-            else:
-                with cols_ui[j]:
-                    st.text_input(f"", value=f"Row {i+1}, Col {j+1}", key=f"r{i}_c{j}", label_visibility="collapsed")
